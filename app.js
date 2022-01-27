e68c8f627d9e63cf8d416299404c8973450d9e9e
bugattie <m.mohsinbarkaati@gmail.com>
2022-01-27 12:54:39 +0500

    basic query and mutation

--- app.js
+++ app.js
@@ -2,14 +2,43 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+
 
 const app = express();
 
 dotenv.config({ path: "./config.env" });
-
+app.use(bodyParser.json());
 
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
+
+app.use('/graphql', graphqlHTTP({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return ['All-night coding', 'Reading'];
+        },
+        createEvent(args) {
+            return args.name;
+        }
+    },
+    graphiql: true
+}))
 
 module.exports = app;
