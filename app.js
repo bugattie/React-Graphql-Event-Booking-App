16424e8f33362c3c2d1a02969fa902f47709d092
Muhammad Mohsin <bugattie>
2022-01-30 11:38:39 +0500

    building relations

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
-const morgan = require('morgan');
-const { graphqlHTTP } = require('express-graphql');
-const { buildSchema } = require('graphql');
+const express = require("express");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
+const Event = require("./models/eventModel");
+const User = require("./models/userModel");
+const bcrypt = require("bcryptjs");
 
 const app = express();
 
 dotenv.config({ path: "./config.env" });
 app.use(bodyParser.json());
 
-if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
-const events = [];
+const user = () => {};
 
-app.use('/graphql', graphqlHTTP({
+app.use(
+  "/graphql",
+  graphqlHTTP({
     schema: buildSchema(`
         type Event {
             _id: ID!
@@ -24,13 +29,26 @@ app.use('/graphql', graphqlHTTP({
             description: String!
             price: Float!
             date: String!
+            creator: User!
+        }
+
+        type User {
+          _id: ID!
+          email: String!
+          password: String
+          createdEvents: [Event!]
         }
 
         input EventInput {
             title: String!
             description: String!
             price: Float!
-            date: String!
+            date: String
+        }
+
+        input UserInput {
+          email: String!
+          password: String!
         }
 
         type RootQuery {
@@ -39,6 +57,7 @@ app.use('/graphql', graphqlHTTP({
 
         type RootMutation {
             createEvent(eventInput: EventInput): Event
+            createUser(userInput: UserInput): User
         }
 
         schema {
@@ -47,23 +66,71 @@ app.use('/graphql', graphqlHTTP({
         }
     `),
     rootValue: {
-        events: () => {
-            return events;
-        },
-        createEvent(args) {
-            const event = {
-                _id: Math.random().toString(),
-                title: args.eventInput.title,
-                description: args.eventInput.description,
-                price: args.eventInput.price,
-                date: args.eventInput.date,
-            };
-            events.push(event);
+      events: async () => {
+        try {
+          const result = await Event.find().populate("creator");
 
-            return event;
+          return result.map((event) => {
+            return {
+              ...event._doc,
+              _id: result.id,
+              creator: {
+                ...event._doc.creator._doc,
+                _id: event._doc.creator.id,
+              },
+            };
+          });
+        } catch (err) {
+          console.log(err);
+          throw err;
         }
+      },
+      createEvent: async (args) => {
+        try {
+          const event = new Event({
+            title: args.eventInput.title,
+            description: args.eventInput.description,
+            price: args.eventInput.price,
+            date: new Date(),
+            creator: "61f53d2993d703f9d1179913",
+          });
+          const createdEvent = await event.save();
+
+          const user = await User.findById("61f53d2993d703f9d1179913");
+
+          if (!user) {
+            throw new Error("User not found");
+          }
+
+          user.createdEvents.push(createdEvent);
+          await user.save();
+
+          return { ...createdEvent._doc, _id: createdEvent.id };
+        } catch (err) {
+          console.log(err);
+          throw err;
+        }
+      },
+
+      createUser: async (args) => {
+        try {
+          const hashedPassword = await bcrypt.hash(args.userInput.password, 12);
+
+          const user = new User({
+            email: args.userInput.email,
+            password: hashedPassword,
+          });
+          const result = await user.save();
+
+          return { ...result._doc, password: null, _id: result.id };
+        } catch (err) {
+          console.log(err);
+          throw err;
+        }
+      },
     },
-    graphiql: true
-}))
+    graphiql: true,
+  })
+);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,5 +1,12 @@
-const app = require('./app');
+const app = require("./app");
+const mongoose = require("mongoose");
 
+mongoose
+  .connect(process.env.DATABASE_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB connected"));
 
 const port = process.env.PORT || 3000;
-app.listen(port, console.log(`Server is listening at ${port}`))
+app.listen(port, console.log(`Server is listening at ${port}`));
